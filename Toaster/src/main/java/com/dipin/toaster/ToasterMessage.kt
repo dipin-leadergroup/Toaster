9ef05dea85ba172b2f094d86c79b7850a95ac0a7
Android Developer <p.dipin@leadergroup.com>
2022-11-28 16:03:45 +0530

    Comment

---
+++ Toaster/src/main/java/com/dipin/toaster/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package com.dipin.toaster
+
+import android.content.Context
+import android.widget.Toast
+
+object ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
